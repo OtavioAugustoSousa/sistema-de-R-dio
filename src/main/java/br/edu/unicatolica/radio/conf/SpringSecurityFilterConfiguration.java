dbfd1441f12964c056f44acf9fcf1d3704a019e0
UNCQLI14394WNT\Aluno Unicatolica <otavioaugustoes@gmail.com>
2016-11-19 13:58:30 -0200

    projeto base

---
+++ src/main/java/br/edu/unicatolica/radio/conf/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,8 @@
+package br.edu.unicatolica.radio.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer
+{
+
+}
